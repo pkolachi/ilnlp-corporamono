928ff51cfe00aec29fc2bf484c98ebc4dd84a824
Prasanth <prasanth.kolachina@cse.gu.se>
2020-06-28 11:19:53 +0200

    add script and README to download Dakshina dataset

---
+++ dakshina/README.txt
@@ -0,0 +1,2 @@
+Dakshina dataset from Google research datasets
+https://github.com/google-research-datasets/dakshina
---
+++ dakshina/init.sh
@@ -0,0 +1,3 @@
+curl --remote-name-all https://storage.googleapis.com/gresearch/dakshina/dakshina_dataset_v1.0.tar
+tar -xf dakshina_dataset_v1.0.tar && mv dakshina_dataset_v1.0 v1.0
+rm -f dakshina_dataset_v1.0.tar
